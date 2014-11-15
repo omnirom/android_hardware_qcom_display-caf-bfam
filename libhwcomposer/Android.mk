490a78e6d147b5bd22d90ddab3e8362851dd4fdf
maxwen <max.weninger@gmail.com>
2014-11-15 16:59:10 +0100

    hwc: added -DSK_SUPPORT_LEGACY_SETCONFIG
    Change-Id: I1aab9d8ab976aa304798adfce4d9b3844fc1d439

--- libhwcomposer/Android.mk
+++ libhwcomposer/Android.mk
@@ -12,7 +12,7 @@ LOCAL_SHARED_LIBRARIES        := $(common_libs) libEGL liboverlay \
                                  libexternal libqdutils libhardware_legacy \
                                  libdl libmemalloc libqservice libsync \
                                  libbinder libmedia libskia libvirtual
-LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdhwcomposer\"
+LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdhwcomposer\" -DSK_SUPPORT_LEGACY_SETCONFIG
 LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
 LOCAL_SRC_FILES               := hwc.cpp          \
                                  hwc_utils.cpp    \
